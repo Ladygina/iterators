ce967a758bdbdc83f4869049bff8c73b8f5f864e
Ladygina <101057953+Ladygina@users.noreply.github.com>
2024-12-27 16:14:26 +0300

    iterator, generator file

---
+++ main.py
@@ -0,0 +1,135 @@
+
+# task 1, task 3
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+
+        self.list_of_list = list_of_list
+        self.result = []
+        self.list_unpack(list_of_list)
+
+    def list_unpack(self, list_of_list):
+
+        for item in list_of_list:
+            if isinstance(item, list):  # если элемент главного списка - список:
+                self.list_unpack(item)  # рекурсивно вызываем метод для вложенных списков
+            else:
+                self.result.append(item)
+
+    def __iter__(self):
+        self.counter = 0  # счетчик
+        return self
+
+    def __next__(self):
+
+        if self.counter == len(self.result):
+            raise StopIteration
+        else:
+            item = self.result[self.counter]
+            self.counter += 1
+            return item
+
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
+
+
+
+
+
+# task 2, task 4
+import types
+def flat_generator(list_of_lists):
+
+    for elem in list_of_lists:
+        if isinstance(elem, list):  # Проверяем, является ли элемент списком
+            #
+            yield from flat_generator(elem)  
+        else:
+            yield  elem
+
+
+def test_2():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_2()
+
+
+def test_3():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+
+if __name__ == '__main__':
+    test_3()
+
+
+def test_4():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator(list_of_lists_2), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_4()
